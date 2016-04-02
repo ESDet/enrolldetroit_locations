1cf4489631597f31f12e9206b7be7d66f3897496
Eric Anderson <anderoy@gmail.com>
2016-04-02 16:32:06 -0400

    Added new icons and on/off filters.

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZXNkIiwiYSI6InBab1ZlUWsifQ.Gwmbd8beRpVIc2kw3x
 
 // Mapbox map load
 
-
+var filterGroup = document.getElementById('filter-group');
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/esd/cih7r65ka002h97maxkalppbt', //stylesheet location custom Scorecard style
@@ -14,9 +14,9 @@ var map = new mapboxgl.Map({
     zoom: 10.5 // starting zoom
 });
 
-// Try to add my GeoJSON from var makers in earlyPrograms.js
+// Add my GeoJSON from var makers in enrollDetroit.js
 
-map.on('style.load', function () {
+map.on('load', function () {
     map.addSource("markers", {
         "type": "geojson",
         "data": markers
@@ -24,68 +24,88 @@ map.on('style.load', function () {
 
     // New addLayer with toggle
 
-   
+    markers.features.forEach(function(feature) {
+        var symbol = feature.properties['marker-symbol'];
+        var layerID = 'loc-' + symbol;
 
-    // Original addLayer
+        // Add a layer for this symbol type if it hasn't been added already.
+        if (!map.getLayer(layerID)) {
+            map.addLayer({
+                "id": layerID,
+                "interactive": true,
+                "type": "symbol",
+                "source": "markers",
+                "layout": {
+                    "icon-image": symbol,
+                    "icon-allow-overlap": true,
+                    "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                    "text-size": 12,
+                    "text-offset": [0, 0.6],
+                },
+                "filter": ["==", "marker-symbol", symbol]
+            });
 
-    map.addLayer({
-        "id": "markers",
-        "interactive": true,
-        "type": "symbol",
-        "source": "markers",
-        "layout": {
-            "icon-image": "orange-18",
-            "icon-allow-overlap": true,
-            "text-field": "{Name}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-size": 12,
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
+            // Add checkbox and label elements for the layer.
+            var input = document.createElement('input');
+            input.type = 'checkbox';
+            input.id = layerID;
+            input.checked = true;
+            filterGroup.appendChild(input);
+
+            var label = document.createElement('label');
+            label.setAttribute('for', layerID);
+            label.textContent = symbol;
+            filterGroup.appendChild(label);
+
+            // When the checkbox changes, update the visibility of the layer.
+            input.addEventListener('change', function(e) {
+                map.setLayoutProperty(layerID, 'visibility',
+                    e.target.checked ? 'visible' : 'none');
+            });
+
+            // Add pop up based on example https://www.mapbox.com/mapbox-gl-js/example/popup-on-click/
+            var popup = new mapboxgl.Popup();
+
+            // When a click event occurs near a marker icon, open a popup at the location of
+            // the feature, with description HTML from its properties.
+            map.on('click', function (e) {
+                map.featuresAt(e.point, {
+                    radius: 18, // Half the marker size (36px).
+                    includeGeometry: true,
+                    layer: layerID
+                }, function (err, features) {
+
+                    if (err || !features.length) {
+                        popup.remove();
+                        return;
+                    }
+
+                    var feature = features[0];
+
+                    // Popuplate the popup and set its coordinates
+                    // based on the feature found.
+                    popup.setLngLat(feature.geometry.coordinates)
+                        .setHTML("<h3>" + feature.properties["Name"] + "</h3>"
+                            + feature.properties.address + "<br/>"
+                            + feature.properties.city + ", " + feature.properties.state + " " + feature.properties.zip + "<br/>"
+                            + "<b>Grades: </b>" + feature.properties.grades + "<br/>"
+                            + "Opens: " + feature.properties.open + "<br/>"
+                            + "Closes: " + feature.properties.close + "<br/>"
+                            )
+                        .addTo(map);
+                });
+            });
+
+            // Use the same approach as above to indicate that the symbols are clickable
+            // by changing the cursor style to 'pointer'.
+            map.on('mousemove', function (e) {
+                map.featuresAt(e.point, {
+                    radius: 18, // Half the marker size (36px).
+                    layer: layerID
+                }, function (err, features) {
+                    map.getCanvas().style.cursor = (!err && features.length) ? 'pointer' : '';
+                });
+            });
         }
-    });
-});
-
-// Add pop up based on example https://www.mapbox.com/mapbox-gl-js/example/popup-on-click/
-
-var popup = new mapboxgl.Popup();
-
-// When a click event occurs near a marker icon, open a popup at the location of
-// the feature, with description HTML from its properties.
-map.on('click', function (e) {
-    map.featuresAt(e.point, {
-        radius: 12, // Half the marker size (24px).
-        includeGeometry: true,
-        layer: 'markers'
-    }, function (err, features) {
-
-        if (err || !features.length) {
-            popup.remove();
-            return;
-        }
-
-        var feature = features[0];
-
-        // Popuplate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(feature.geometry.coordinates)
-            .setHTML("<h3>" + feature.properties["Name"] + "</h3>"
-                + feature.properties.address + "<br/>"
-                + feature.properties.city + ", " + feature.properties.state + " " + feature.properties.zip + "<br/>"
-                + "<b>Grades: </b>" + feature.properties.grades + "<br/>"
-                + "Opens: " + feature.properties.open + "<br/>"
-                + "Closes: " + feature.properties.close + "<br/>"
-                )
-            .addTo(map);
-    });
-});
-
-// Use the same approach as above to indicate that the symbols are clickable
-// by changing the cursor style to 'pointer'.
-map.on('mousemove', function (e) {
-    map.featuresAt(e.point, {
-        radius: 12, // Half the marker size (24px).
-        layer: 'markers'
-    }, function (err, features) {
-        map.getCanvas().style.cursor = (!err && features.length) ? 'pointer' : '';
     });
 });
